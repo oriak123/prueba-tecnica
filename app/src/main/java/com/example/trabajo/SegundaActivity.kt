8413e64c57cbfcceb7ed43ebd108dd7d2c473ad6
Jordan Tafur <jatafurd1@hotmail.com>
2022-03-31 21:21:43 -0500

    first commit

---
+++ app/src/main/java/com/example/trabajo/SegundaActivity.kt
@@ -0,0 +1,75 @@
+package com.example.trabajo
+
+import android.content.Intent
+import android.content.res.Configuration
+import android.graphics.drawable.GradientDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import android.widget.Toolbar
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+
+class SegundaActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var drawer: DrawerLayout
+    private lateinit var toggle: ActionBarDrawerToggle
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.segunda_activity)
+
+        val toolbar: androidx.appcompat.widget.Toolbar =findViewById(R.id.toolbar_main)
+        setSupportActionBar(toolbar)
+
+        drawer = findViewById(R.id.drawer_layout)
+
+        toggle = ActionBarDrawerToggle(this,drawer,toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer.addDrawerListener(toggle)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+
+        val navigationView: NavigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+    }
+    fun volver (view: View){
+
+        startActivity(Intent(this,PrimeraActivity::class.java))
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId){
+            R.id.nav_item_one -> Toast.makeText(this,"Inicio",Toast.LENGTH_SHORT).show()
+            R.id.nav_item_two -> Toast.makeText(this,"¿Quienes somos?",Toast.LENGTH_SHORT).show()
+            R.id.nav_item_three -> Toast.makeText(this,"Productos",Toast.LENGTH_SHORT).show()
+            R.id.nav_item_four -> Toast.makeText(this,"Quejas y reclamos",Toast.LENGTH_SHORT).show()
+        }
+        drawer.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        toggle.syncState()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        toggle.onConfigurationChanged(newConfig)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+}
